7fd655b9fa47f7f623db86c934901c95310e8223
Gian Merlino <gianmerlino@gmail.com>
2014-04-17 11:34:43 -0700

    HttpBeam: ServiceExceptions are transient, too.

--- src/main/scala/com/metamx/tranquility/beam/HttpBeam.scala
+++ src/main/scala/com/metamx/tranquility/beam/HttpBeam.scala
@@ -114,6 +114,7 @@ class HttpBeam[A: Timestamper](
       ifException[com.twitter.finagle.RequestException],
       ifException[com.twitter.finagle.ChannelException],
       ifException[com.twitter.finagle.TimeoutException],
+      ifException[com.twitter.finagle.ServiceException],
       ifException[org.jboss.netty.channel.ChannelException],
       ifException[org.jboss.netty.channel.ConnectTimeoutException],
       ifException[org.jboss.netty.handler.timeout.TimeoutException]
