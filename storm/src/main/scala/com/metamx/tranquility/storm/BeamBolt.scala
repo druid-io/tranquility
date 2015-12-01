75c2eefa3418791e2d23fd869d15baeea98e6877
Gian Merlino <gianmerlino@gmail.com>
2015-12-01 14:11:29 -0800

    Storm BeamBolt: Use Tranquilizer instead of Beam.

--- storm/src/main/scala/com/metamx/tranquility/storm/BeamBolt.scala
+++ storm/src/main/scala/com/metamx/tranquility/storm/BeamBolt.scala
@@ -25,17 +25,19 @@ import backtype.storm.tuple.Tuple
 import com.metamx.common.scala.Logging
 import com.metamx.common.scala.concurrent.abortingThread
 import com.metamx.tranquility.beam.Beam
+import com.metamx.tranquility.tranquilizer.MessageDroppedException
+import com.metamx.tranquility.tranquilizer.Tranquilizer
 import com.twitter.util.Await
 import com.twitter.util.Future
 import java.{util => ju}
 import scala.collection.mutable.ArrayBuffer
 
 /**
- * A Storm Bolt for using a Beam to propagate tuples.
- *
- * @param beamFactory Factory for creating the Beam we will use.
- * @param batchSize Maximum number of events to send per call to Beam.propagate.
- */
+  * A Storm Bolt for using a Beam to propagate tuples.
+  *
+  * @param beamFactory Factory for creating the Beam we will use.
+  * @param batchSize Maximum number of events to send per call to Beam.propagate.
+  */
 class BeamBolt[EventType](
   beamFactory: BeamFactory[EventType],
   batchSize: Int
@@ -43,70 +45,44 @@ class BeamBolt[EventType](
 {
   def this(beamFactory: BeamFactory[EventType]) = this(beamFactory, 2000)
 
-  @volatile private var lock      : AnyRef             = null
-  @volatile private var running   : Boolean            = false
-  @volatile private var buffer    : ArrayBuffer[Tuple] = new ArrayBuffer[Tuple]()
-  @volatile private var sendThread: Thread             = null
-  @volatile private var beam      : Beam[EventType]    = null
+  @volatile private var running     : Boolean                 = false
+  @volatile private var collector   : OutputCollector         = null
+  @volatile private var tranquilizer: Tranquilizer[EventType] = null
 
   override def prepare(conf: ju.Map[_, _], context: TopologyContext, collector: OutputCollector) {
-    require(lock == null, "WTF?! Already initialized, but prepare was called anyway.")
-    lock = new AnyRef
-    beam = beamFactory.makeBeam(conf, context)
-    sendThread = abortingThread {
-      try {
-        while (!Thread.currentThread().isInterrupted && running) {
-          val tuples = lock.synchronized {
-            while (buffer.isEmpty) {
-              lock.wait()
-            }
-            val _buffer = buffer
-            buffer = new ArrayBuffer[Tuple]()
-            _buffer
-          }
-
-          val ok: Boolean = try {
-            val futures = (for (batch <- tuples.grouped(batchSize)) yield {
-              beam.propagate(batch.map(_.getValue(0).asInstanceOf[EventType]))
-            }).toList
-            val sent: Int = Await.result(Future.collect(futures.toList)).sum
-            log.debug("%s: Flushed %,d, ignored %,d messages.", beam, sent, tuples.size - sent)
-            true
-          }
-          catch {
-            case e: Exception =>
-              log.warn(e, "%s: Failed to send %,d messages.", beam, tuples.size)
-              false
-          }
-
-          if (ok) {
-            tuples foreach collector.ack
-          } else {
-            tuples foreach collector.fail
-          }
-        }
-      }
-      catch {
-        case e: InterruptedException => // Exit peacefully
-      }
-    }
-    sendThread.setDaemon(true)
-    sendThread.setName("BeamSendThread-%s" format beam)
+    require(this.collector == null, "WTF?! Already initialized, but prepare was called anyway.")
+    this.collector = collector
+    this.tranquilizer = Tranquilizer.create(
+      beamFactory.makeBeam(conf, context),
+      batchSize,
+      Tranquilizer.DefaultMaxPendingBatches,
+      Tranquilizer.DefaultLingerMillis
+    )
+    this.tranquilizer.start()
     running = true
-    sendThread.start()
   }
 
   override def execute(tuple: Tuple) {
-    lock.synchronized {
-      buffer += tuple
-      lock.notifyAll()
+    tranquilizer.send(tuple.getValue(0).asInstanceOf[EventType]) onSuccess { res =>
+      collector.synchronized {
+        collector.ack(tuple)
+      }
+    } onFailure {
+      case e: MessageDroppedException =>
+        collector.synchronized {
+          collector.ack(tuple)
+        }
+
+      case e =>
+        collector.synchronized {
+          collector.fail(tuple)
+        }
     }
   }
 
   override def cleanup() {
     running = false
-    sendThread.interrupt()
-    Await.result(beam.close())
+    tranquilizer.stop()
   }
 
   override def declareOutputFields(declarer: OutputFieldsDeclarer) {
